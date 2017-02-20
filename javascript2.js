2f09efe303870772ba583848a18f5844b80a467b
Robert Harężlak <harezlak.robert7@gmail.com>
2017-02-20 22:56:16 +0100

    counting votes with ARRAY.reduce

---
+++ javascript2.js
@@ -0,0 +1,33 @@
+var votes = [
+    "angular",
+    "angular",
+    "react",
+    "react",    
+    "react",
+    "react",
+    "react",
+    "ember",
+    "ember",
+    "ember",
+    "vanilla"
+];
+
+var initialValue = {};
+
+var reducer = function(tally, vote) {
+    if(!tally[vote]) {
+        tally[vote] = 1;
+    }   else {
+        tally[vote] = tally[vote] + 1;
+    }
+
+    return tally;
+};
+
+var result = votes.reduce(reducer, initialValue);
+
+console.log(result);
+
+
+
+
